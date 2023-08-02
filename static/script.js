a6d9de463eb6a8fe4b6391c307026c14c2853dfa
Gnunroh <91159416+Gnunroh@users.noreply.github.com>
2023-08-02 13:39:02 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-availableImages=['images/CM_Vorlage.png','images/cm_128bit.png','images/cm_kopf.png','images/cm_kopf32.png','images/cm_trainer.png','images/cm_vorlage2.png','images/cm_trainer.png','images/cm_vorlage2.png','images/CM_Vorlage.png','images/cm_128bit.png','images/cm_kopf.png','images/cm_kopf32.png','images/cm_trainer.png','images/cm_vorlage2.png','images/cm_trainer.png','images/cm_vorlage2.png']
+availableImages=['images/CM_Vorlage.png','images/cm_128bit.png','images/cm_kopf.png','images/cm_kopf32.png','images/cm_trainer.PNG','images/cm_vorlage2.png','images/adiran_1.PNG']
 const maindiv = document.getElementById("boardgame")
 const scorediv = document.getElementById("score");
 function start(){
