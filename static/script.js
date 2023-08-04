6bef5b6e522a8c6f61e5be9e65b2955c2c0ff94a
Gnunroh <91159416+Gnunroh@users.noreply.github.com>
2023-08-04 16:07:33 +0200

    Added mobile view

--- static/script.js
+++ static/script.js
@@ -1,7 +1,20 @@
 availableImages=['images/a_1.jpg','images/a_2.jpg','images/a_3.jpg','images/a_4.jpg','images/a_5.jpg','images/a_6.jpg','images/a_7.jpg','images/a_8.jpg','images/a_9.jpg','images/a_10.jpg','images/a_11.jpg','images/a_12.jpg','images/a_13.jpg','images/a_14.jpg','images/a_15.jpg','images/a_16.jpg','images/a_1.jpg','images/a_2.jpg','images/a_3.jpg','images/a_4.jpg','images/a_5.jpg','images/a_6.jpg','images/a_7.jpg','images/a_8.jpg','images/a_9.jpg','images/a_10.jpg','images/a_11.jpg','images/a_12.jpg','images/a_13.jpg','images/a_14.jpg','images/a_15.jpg','images/a_16.jpg']
 const maindiv = document.getElementById("boardgame")
 const scorediv = document.getElementById("score");
+const screenHeight = window.innerHeight;
+const screenWidth = window.innerWidth;
 let password = "undefined";
+
+function setTileSize(height,width) {
+    if(width >= height) {
+        TileWidth = width * 0.85 / 8;
+        return TileWidth;
+    }
+    else {
+        TileWidth = height * 0.85 / 8;
+        return TileWidth;
+    }
+}
 
 function protectpasscode() {
     const result = document.getElementById("tutorial").value;
@@ -22,28 +35,27 @@ function protectpasscode() {
        }
     }
 function start(){
+    tileWidth = setTileSize(screenHeight, screenWidth);
+    console.log(tileWidth);
     passwordCorrect = protectpasscode();
     console.log(passwordCorrect);
     if (passwordCorrect=="False"){
         return;
     }
     let moves =0;
-    var row = document.createElement('div')
-    ImagesCopy= JSON.parse(JSON.stringify( availableImages))
+    ImagesCopy= JSON.parse(JSON.stringify( availableImages));
     for(let j=1;j<=32;j++){
         var div = document.createElement('div');
-        div.setAttribute('class','imgdiv')
+        div.setAttribute('class','imgdiv');
+        div.setAttribute('style', 'height:' + tileWidth + 'px; width:' + tileWidth + 'px')
         var image = document.createElement('img')
         randomImg = ImagesCopy.splice(Math.floor(Math.random() * ImagesCopy.length),1);
         image.setAttribute('src',randomImg);
-        image.setAttribute('class','hide')
+        image.setAttribute('class','hide');
+        image.setAttribute('style', 'height:' + tileWidth + 'px; width:' + tileWidth + 'px')
         div.appendChild(image)
-        row.appendChild(div);
-        
-        if(j%8==0){
-            document.getElementById('boardgame').append(row)
-            row = document.createElement('div')
-        }
+        document.getElementById('boardgame').append(div);
+
  
         div.addEventListener('click',function(event){
             moves++;
